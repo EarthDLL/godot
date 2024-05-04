9dbd183d21298ffacce22f084b651374a2ad31c4
EarthDLL <100025289+EarthDLL@users.noreply.github.com>
2024-05-04 19:37:19 +0800

    Update custom.py

--- custom.py
+++ custom.py
@@ -1,9 +1,1 @@
-# Generated using https://godot-build-options-generator.github.io
-
-extra_suffix = "custom_build_for_propkiller"
 disable_3d = "yes"
-module_bmp_enabled = "no"
-module_openxr_enabled = "no"
-module_opus_enabled = "no"
-module_theora_enabled = "no"
-module_webxr_enabled = "no"
