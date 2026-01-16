d28f5db8bc10769d1dedd9a8d491b7e13fc11f1b
EarthDLL <100025289+EarthDLL@users.noreply.github.com>
2026-01-17 03:40:52 +0800

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,9 @@
+target = "template_release"
+tools = "no"  # 不编译编辑器，只做导出模板
+disable_3d = "yes"  # 2D项目关3D
+# 禁用模块（按项目需求增删）
+module_enet_enabled = "no"
+module_websocket_enabled = "no"
+module_svg_enabled = "no"
+module_theora_enabled = "no"
+build_profile = "./build_profile.gdbuild"  # 之前保存的配置
